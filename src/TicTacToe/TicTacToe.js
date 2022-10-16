ecf456e3ed153ef8d420534218d752422d6a8ba6
Muath <muath.khalifa@yahoo.com>
2022-10-16 22:06:16 +0100

    winning conditions

--- src/TicTacToe/TicTacToe.js
+++ src/TicTacToe/TicTacToe.js
@@ -3,24 +3,79 @@ import "./TicTacToe.css"
 
 const TicTacToe = () => {
 
-    const [turn, setTurn] = useState("x")
-    
-    const handleClick = (num) =>{
-            if(turn == "x"){
-                setTurn('o')
-            }else{
-                setTurn("x")
-            }
+    const [turn, setTurn] = useState("x");
+    const [cells, setCells] = useState(Array(9).fill(''));
+    const [winner, setWinner] = useState(null)
+
+    const checkForWinnder = (squares) =>{
+        let combos = {
+            across: [
+            [0,1,2],
+            [3,4,5],
+            [6,7,9],
+        ],
+            down: [
+                [0,3,6],
+                [1,4,7],
+                [2,5,9],
+            ],
+            diagnol: [
+                [0,4,9],
+                [6,4,2]
+            ]
         }
 
+        for(let combo in combos){
+            combos[combo].forEach((pattern) =>{
+                if(
+                    squares[pattern[0]] === '' ||
+                    squares[pattern[1]] === '' ||
+                    squares[pattern[2]] === '' 
+                ){
+
+                }
+                    else if (
+                        squares[pattern[0]] === squares[pattern[1]] &&
+                        squares[pattern[1]] === squares[pattern[2]] 
+                    ) 
+                    
+                    {
+                        setWinner(squares[pattern[0]]);
+                    }
+                
+                console.log(pattern)
+            })
+        }
+    }
+    
+    const handleClick = (num) =>{
+        if(cells[num] !== ""){
+            // alert("already clicked")
+            return;
+        }
+        let squares = [...cells];
+
+            if(turn == "x"){
+                squares[num] = "x";
+                setTurn('o')
+            }else{
+                squares[num] = "o"
+                setTurn("x")
+            }
+            checkForWinnder(squares);
+            setCells(squares);
+            console.log(squares)
+        }
+
+
     const Cell = ({num}) =>{
-        return <td onClick={() => handleClick(num)}>-</td>
+        return <td onClick={() => handleClick(num)}>{cells[num]}</td>
     }
   return (
     <div className='container'>
 
-        <p>{turn}</p>
         <table>
+        {/* Turn: {turn} */}
             <tbody>
                 <tr>
                     <Cell num={0} />
